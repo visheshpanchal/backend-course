6d20cdcea6cde25c6f8ce4f68e714d5f53beeebc
Vishesh Panchal <visheshpanchal145@mail.com>
2022-09-15 16:55:37 +0530

    Task 8 project code clean

---
+++ nodejs/routes.js
@@ -0,0 +1,48 @@
+const fs = require("fs");
+
+let requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title><head>");
+
+    if (fs.existsSync("./message.txt")) {
+      console.log("Work This Function");
+      let data = fs.readFileSync("./message.txt", "utf-8").split("\n");
+
+      for (let dt of data) {
+        res.write("<div>");
+        res.write(dt);
+        res.write("</div>");
+      }
+    }
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.appendFile("./message.txt", "\n" + message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  }
+};
+
+module.exports = {
+  handler: requestHandler,
+  comment: "To Handle Request Response",
+};
--- nodejs/task8.js
+++ nodejs/task8.js
@@ -1,48 +1,6 @@
 const http = require("http");
-const fs = require("fs");
-const readlines = require("readline");
-const events = require("events");
+const routes = require("./routes");
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Enter Message</title><head>");
-
-    if (fs.existsSync("./message.txt")) {
-      console.log("Work This Function");
-      let data = fs.readFileSync("./message.txt", "utf-8").split("\n");
-
-      for (let dt of data) {
-        res.write("<div>");
-        res.write(dt);
-        res.write("</div>");
-      }
-    }
-    res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
-    );
-
-    res.write("</html>");
-    return res.end();
-  }
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-    return req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-      fs.appendFile("./message.txt", "\n" + message, (err) => {
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      });
-    });
-  }
-});
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
