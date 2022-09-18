d172bab9617e2665f705f4db83142ab60911171f
Vishesh Panchal <visheshpanchal145@mail.com>
2022-09-18 20:27:35 +0530

    Modifying paths  using router

--- startwiith express js framework/app1/app.js
+++ startwiith express js framework/app1/app.js
@@ -2,24 +2,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+const adminRoutes = require("./routes/admin");
+const shopRoute = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title" placeholder="name"><input type="text" placeholder="size" name="size"><button type="submit">Add Product</button></form>'
-  );
-});
+app.use("/admin", adminRoutes);
+app.use(shopRoute);
 
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not Found</h1>");
 });
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from Express!</h1>");
-});
-
 app.listen(3000, () => {
   console.log("Listening From 3000");
 });
